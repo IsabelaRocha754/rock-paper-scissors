5ba63bcf26a1f2666989b0ed789f01f289d10843
Isabela <isarocha0012@gmail.com>
2026-04-25 11:51:36 -0300

    Function for the human's choice
    This is a simple function that prompts the user for a string (rock, paper, scissors) and returns the user's input

--- javascript.js
+++ javascript.js
@@ -3,3 +3,10 @@ function getComputerChoice(max){
 }
 
 console.log(getComputerChoice(3));
+
+function getHumanChoice(){
+    let string = prompt("Play: ");
+    return string;
+}
+
+console.log(getHumanChoice());
