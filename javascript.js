e92fb8e8a9b34e85579a64654ef1c2fe61ff6fc0
Isabela <isarocha0012@gmail.com>
2026-05-12 21:10:25 -0300

    Logic for multiple rounds
    If either the player or the computer reaches 5 points, the choice buttons are disabled.
    The play again button sets the scores to 0 and enables the buttons for a new round

--- javascript.js
+++ javascript.js
@@ -6,47 +6,74 @@ function getComputerChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
-const display = document.getElementById("display");
-
 //div for displaying humanChoice and computerChoice
 const choices = document.createElement("div");
 choices.classList.add("choices");
 document.body.appendChild(choices);
 
+//div for displaying the score each round
+const score = document.createElement("div");
+score.classList.add("score");
+document.body.appendChild(score);
+
+//div for displaying who won the round
 const result = document.createElement("div");
 result.classList.add("result");
 document.body.appendChild(result);
+
+//button to restart the game
+const playAgain = document.querySelector("#playAgain");
+playAgain.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    score.textContent = `${humanScore} X ${computerScore}`;
+    choices.textContent = ``;
+    result.textContent = ``;
+
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+});
 
 function playRound(humanChoice, computerChoice){
     if ((humanChoice == 'rock' && computerChoice == 'paper')||
         (humanChoice == 'paper' && computerChoice == 'scissors')||
         (humanChoice == 'scissors' && computerChoice == 'rock')){
             choices.textContent = `Your choice: ${humanChoice} | Computer's choice: ${computerChoice}`;
-            result.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+            result.textContent = `You lose this round :( ${computerChoice} beats ${humanChoice}`;
             computerScore += 1;
     }
     else if ((humanChoice == 'rock' && computerChoice == 'scissors')||
             (humanChoice == 'paper' && computerChoice == 'rock')||
             (humanChoice == 'scissors' && computerChoice == 'paper')){
                 choices.textContent = `Your choice: ${humanChoice} | Computer's choice: ${computerChoice}`;
-                result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+                result.textContent = `You win this round! ${humanChoice} beats ${computerChoice}`;
                 humanScore += 1;
     }
     else{
         choices.textContent = `Your choice: ${humanChoice} | Computer's choice: ${computerChoice}`;
         result.textContent = "It's a tie!"
     }
+
+    score.textContent = `${humanScore} X ${computerScore}`;
+
+    checkWinner(humanScore, computerScore);
 }
 
 function checkWinner(humanScore, computerScore){
-    if (humanScore > computerScore){
-        console.log("You win the game!");
+    if (humanScore == 5){
+        result.textContent = `You won the game! \nFinal score: ${humanScore} X ${computerScore}`;
+
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
     }
-    else if (humanScore < computerScore){
-        console.log("You lose the game :(");
-    }
-    else{
-        console.log("It's a tie!");
+    else if (computerScore == 5){
+        result.textContent = `You lost the game:( \nFinal score: ${humanScore} X ${computerScore}`;
+
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
     }
 };
 
